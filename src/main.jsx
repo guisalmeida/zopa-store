fd76dabdb740d13203732e31667c23364cab8da3
guisalmeida <guisalmeida.dev@gmail.com>
2023-11-02 18:56:09 -0300

    feat: add redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
 
-import { store } from './store/store.js'
+import { store, persistor } from './store/store.js'
 
 import GlobalStyles from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <GlobalStyles />
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <GlobalStyles />
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- src/store/store.js
+++ src/store/store.js
@@ -1,26 +1,30 @@
 import { compose, legacy_createStore, applyMiddleware } from 'redux'
 import { rootReducer } from './rootReducer'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
-const loggerMiddleware = store => next => action => {
-  if (!action.type) {
-    return next(action)
-  }
-
-  console.log('type: ', action.type)
-  console.log('payload: ', action.payload)
-  console.log('currentState: ', store.getState())
-
-  next(action)
-
-  console.log('next state: ', store.getState())
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
 }
 
-const middleWares = [loggerMiddleware]
+const middleWares = []
 
-const composedEnhancers = compose(applyMiddleware(...middleWares))
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const composeEnhancer =
+  (import.meta.env.MODE === 'development' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares))
 
 export const store = legacy_createStore(
-  rootReducer,
+  persistedReducer,
   undefined,
   composedEnhancers,
 )
+
+export const persistor = persistStore(store)
