7e28689e2f1996d068d0b35d682a215db67356ca
guisalmeida <guisalmeida.dev@gmail.com>
2023-11-01 22:12:32 -0300

    chore: log state with middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,26 @@
-import { legacy_createStore } from 'redux'
+import { compose, legacy_createStore, applyMiddleware } from 'redux'
 import { rootReducer } from './rootReducer'
 
-export const store = legacy_createStore(rootReducer)
+const loggerMiddleware = store => next => action => {
+  if (!action.type) {
+    return next(action)
+  }
+
+  console.log('type: ', action.type)
+  console.log('payload: ', action.payload)
+  console.log('currentState: ', store.getState())
+
+  next(action)
+
+  console.log('next state: ', store.getState())
+}
+
+const middleWares = [loggerMiddleware]
+
+const composedEnhancers = compose(applyMiddleware(...middleWares))
+
+export const store = legacy_createStore(
+  rootReducer,
+  undefined,
+  composedEnhancers,
+)
