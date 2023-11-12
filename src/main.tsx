2d53eee42a5efde7d1a5b28e3e262ad95a8cefb3
guisalmeida <guisalmeida.dev@gmail.com>
2023-11-12 12:51:43 -0300

    chore: set up TS

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
@@ -11,7 +11,7 @@ import { store, persistor } from './store/store.js'
 
 import GlobalStyles from './styles/global.js'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
