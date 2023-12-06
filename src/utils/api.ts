af5f7bc56f90423ea062a98b68cc8c2ca1a94ec6
guisalmeida <guisalmeida.dev@gmail.com>
2023-12-06 18:43:43 -0300

    chore: create api login

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,22 +1,53 @@
 import { TProduct } from '../types'
 import axios from 'axios'
 
+const BASE_URL = 'http://localhost:5000/api'
+const local = JSON.parse(localStorage.getItem('persist:root') || '')
+const user = local?.user
+const currentUser = user && JSON.parse(user).currentUser
+const TOKEN = currentUser?.accessToken
+
+export const publicRequest = axios.create({
+  baseURL: BASE_URL,
+})
+
+export const userRequest = axios.create({
+  baseURL: BASE_URL,
+  headers: { token: `Bearer ${TOKEN}` },
+})
+
 export const getProductsCollection = async (
   category = '',
   limit = 0,
 ): Promise<TProduct[] | void> => {
-  const productsUrl = 'http://localhost:5000/api/products'
   const params: { category?: string; limit?: number } = {}
 
   if (category) params.category = category
   if (limit) params.limit = limit
 
   try {
-    const { data } = await axios.get<TProduct[]>(productsUrl, {
+    const { data } = await publicRequest.get<TProduct[]>('/products', {
       params,
     })
 
     return data
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return console.log(error.message)
+    } else {
+      return console.log(error as Error)
+    }
+  }
+}
+
+export const signInUserWithEmailAndPassword = async (
+  email: string,
+  password: string,
+) => {
+  if (!email || !password) return
+
+  try {
+    return await publicRequest.post('/auth/login', { email, password })
   } catch (error) {
     if (axios.isAxiosError(error)) {
       return console.log(error.message)
